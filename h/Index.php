1f61a95f5e7589adbf2ea66388be0d772325377e
pattarawadee05 <66010914055@msu.ac.th>
2026-02-04 15:20:16 +0700

    a

--- h/Index.php
+++ h/Index.php
@@ -13,5 +13,26 @@ Username <input type="text" name="auser" autofocus required> <br>
 <button type="submit" name"Sunmit">LOGIN</button>
 </form>
 
+<?php
+if(isset($_POST['Submit'])) {
+	include_once("connectdb.php");
+	$sql = "SELECT * FROM admin WHERE a_username='{$_POST['auser']}' AND a_password='{$_POST['apwd']}' LIMIT 1" ;
+	$rs = mysqli_query($conn, $sql);
+	$num = mysqli_num_rows($rs);
+
+	if($num == 1) {
+		$data =mysqli_fetch_array($rs);
+		$_SESSION['aid'] = $data['a_id']
+		$_SESSION['aname'] = $data['a_name']
+		echo "<script>" ;
+		echo "window.location='index2.php';" ;
+		echo "</script>" ;
+	} else {
+		echo "<script>" ;
+		echo "alert('Username sinv Password ไม่ถูกต้อง');" ;
+		echo "</script>" ;
+	}
+}
+?>
 </body>
 </html>
