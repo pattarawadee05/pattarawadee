63a23e92bb299e8ee23895fe2df6443c9b1d6035
pattarawadee05 <66010914055@msu.ac.th>
2026-02-04 15:38:26 +0700

    kj

---
+++ h/b.php
@@ -0,0 +1,25 @@
+<?php
+    session_start
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>66010914057 มัทนา รัตนแสง(น้ำฝน)</title>
+</head>
+
+<body>
+    <h1>b.php</h1>
+
+    <?php
+        echo @$_SESSION['name'] ."<br>";
+        echo @$_SESSION['nickname'] ."<br>";
+        echo @$_SESSION['p1'] ."<br>";
+        echo @$_SESSION['p2'] ."<br>";
+    ?>
+
+</body>
+</html>
+
+
+
---
+++ h/e.php
@@ -0,0 +1,23 @@
+<?php
+    session_start
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>66010914057 มัทนา รัตนแสง(น้ำฝน)</title>
+</head>
+
+<body>
+    <h1>b.php</h1>
+
+    <?php
+        unset ($_SESSION['name']);
+        unset ($_SESSION['p1']);
+    ?>
+
+</body>
+</html>
+
+
+
