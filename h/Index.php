fe4e829a97d4f4210c5b8f2fcf5a2f64978d2cd8
pattarawadee05 <66010914055@msu.ac.th>
2026-02-04 14:49:06 +0700

    a

---
+++ h/Index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ภัทรวดี ขามประโคน (การ์ตูน)</title>
+</head>
+
+<body>
+<h1>เข้าสู่ระบบหลังบ้าน - ภัทรวดี</h1>
+
+<form method="post" action="">
+Username <input type="text" name="auser" autofocus required> <br>
+<button type="submit" name"Sunmit">LOGIN</button>
+</form>
+
+</body>
+</html>
