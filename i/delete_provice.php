82d81e88f603b928e12db6088e5c67f77e46979e
pattarawadee05 <66010914055@msu.ac.th>
2026-02-17 20:52:12 +0700

    a

---
+++ i/delete_provice.php
@@ -0,0 +1,17 @@
+<meta charset="utf-8">
+<?php
+include_once(connectdb.php);
+
+$id = $_GET['id'];
+$ext = $_GET['ext'];
+
+$sql = "DELETE FROM provices WHERE p_id='{$id}'";
+mysqli_query($conn, $sql) or die ("ลบข้อมูลไม่ได้");
+
+unlink("images/".$id.".".$ext);
+
+echo "<script>" ;
+echo "window.location='b.php';" ;
+echo "</script>" ;
+
+?>
