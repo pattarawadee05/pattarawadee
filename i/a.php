cdb719ceaee6894224c7b9d53736e822fe399811
pattarawadee05 <66010914055@msu.ac.th>
2026-02-17 19:35:26 +0700

    a

--- i/a.php
+++ i/a.php
@@ -2,34 +2,24 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>ภัทรวดี ขามประโคน(การ์ตูน)</title>
+<title>66010914055 ภัทรวดี ขามประโคน(การ์ตูน)</title>
 </head>
 
 <body>
-<h1>งาน i - ภัทรวดี ขามประโคน(การ์ตูน)</h1>
+<h1>งานi --66010914055 ภัทรวดี ขามประโคน(การ์ตูน)</h1>
 
 <form method="post" action="">
     ชื่อภาค <input type="text" name="rname" autofocus required>
-    <button type="submit" name="Submit">บันทึก</button>
-    </form><br><br>
-    
+    <button type="submit" name="Submit"> บันทึก </button>
+</form><br><br>
+
 <?php
 if(isset($_POST['Submit'])){
     include_once("connectdb.php");
     $rname = $_POST['rname'];
-    $sql = "INSERT INTO regions (r_id, r_name) VALUES (NULL,'{$rame}')";
+    $sql2 = "INSERT INTO regions (r_id, r_name) VALUES (NULL, '{$rname}')";
     mysqli_query($conn,$sql2) or die ("เพิ่มข้อมูลไม่ได้");
 }
-
-<?php
-include_once("connectdb.php");
-$sql = "SELECT * FROM regions";
-$rs = mysqli_query($conn,$sql);
-while($data = mysqli_fetch_array($rs)){
-    ehco $data['r_id'] . "<br>";
-    ehco $data['r_name'] . "<hr>";
-    }
-mysqli_close($conn);
 ?>
 
 <table border="1">
@@ -39,18 +29,21 @@ mysqli_close($conn);
         <th>ลบ</th>
     </tr>
 <?php
-include_once("connectdb.php")
+include_once("connectdb.php");
 $sql = "SELECT * FROM regions";
 $rs = mysqli_query($conn,$sql);
 while($data = mysqli_fetch_array($rs)){
 ?>
     <tr>
-        <td><?php ehco $data['r_id'] ; ?></td>
-        <td><?php ehco $data['r_name'] ; ?></td>
-        <td width="80" align="center"><img src="../img/delete.jpg" width="20"></td>
+        <td><?php echo $data['r_id']; ?></td>
+        <td><?php echo $data['r_name']; ?></td>
+        <td width="80" align="center"><a href="delete_region.php?id=<?php echo $data['r_id']; ?>" onClick="return confirm('ยืนยันการลบ?');"><img src="images/delete.jpg" width="20"></a></td>
     </tr>
-    <?php} ?>
+<?php } ?>
 </table>
 
+<?php
+mysqli_close($conn);
+?>
 </body>
 </html>
