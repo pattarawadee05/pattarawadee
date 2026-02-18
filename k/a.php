047c4debd3f8e1322f168f7e21681cb9d64c4dd8
pattarawadee05 <66010914055@msu.ac.th>
2026-02-18 16:55:04 +0700

    a

---
+++ k/a.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>66010914055 ภัทรวดี ขามประโคน (การ์ตูน)</title>
+</head>
+
+<h1>งาน k 66010914055 ภัทรวดี ขามประโคน (การ์ตูน)</h1>
+<button type="button" class="btn btn-นิสิต">นิสิต</button>
+<img src="images/1.jpg" width="400">
+<button type="button" class="btn btn-อาจารย์">อาจารย์</button>
+<img src="images/2.jpg" width="400">
+
+<body>
+</body>
+</html>
